d88e9dad1de545998c3e387e5f8bf37f82c0fd80
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-10 16:14:49 -0600

    v1

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import LoginView from './src/LoginView';
+import HomeView from './src/HomeView';
+import ArtistPage from './src/ArtistPage';
+import {Actions, Scene, Router} from 'react-native-router-flux';
+
+const scenes = Actions.create(
+  <Scene key="root" >
+    <Scene key="login" component={LoginView} hideNavBar/>
+    <Scene key="home" component={HomeView}  title="Lista de artistas"/>
+    <Scene key="artistPage" component={ArtistPage} title="Artista"/>
+  </Scene>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Router scenes={scenes}/>
+  )
+
+  
+   
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/api-client.js
@@ -0,0 +1,23 @@
+const apiKey = 'key';
+const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=${apiKey}&format=json`;
+
+export default function getMusicData() {
+    console.log('api');
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }) 
+        .then(response => response.json())
+        .then(data => data.topartists.artist)
+        .then(artists => artists.map(artist => {
+            return {
+                id: artist.mbid,
+                name: artist.name,
+                image: artist.image[3]['#text']
+            }
+        }));
+}
+
